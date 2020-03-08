62c0dbd3653f65740736228acde6b1a2b99dde6a
Bret Gibson <bret.gibson@gmail.com>
2020-03-08 02:00:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,7 +27,6 @@ end
 def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each do |ele|
-    puts ele
     if snacks.include?(ele)
       return ele
     else
