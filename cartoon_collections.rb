ed83690e41351556d185dba557461db8e6cbcc53
Bret Gibson <bret.gibson@gmail.com>
2020-03-08 01:36:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,5 +28,6 @@ end
 def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
   puts snacks.include?(cheese_types[0])
-  snacks.include?(cheese_types[0])
+  if snacks.include?(cheese_types[0])
+    return cheese_types[0]
 end
