4ffba78dca216dec8d2bd3467dba706305697b07
Bret Gibson <bret.gibson@gmail.com>
2020-03-08 01:23:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,10 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(short_words)
-  
+  short_words.each do |ele|
+
+    
+  end
 end
 
 def find_the_cheese# code an argument here
