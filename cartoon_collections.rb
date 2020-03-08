1332ca7888323c52e6ffae1ea3044efc96daee34
Bret Gibson <bret.gibson@gmail.com>
2020-03-08 01:11:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,6 @@ def summon_captain_planet(planeteer_calls)
   newArr = planeteer_calls.map do |ele|
     ele.capitalize
   end
-  newArr.push("!")
-  puts newArr
 end
 
 def long_planeteer_calls# code an argument here
