2358996436c407bf7246b3bc47e36f2152ee70f2
Bret Gibson <bret.gibson@gmail.com>
2020-03-08 01:09:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,9 @@ end
 end
 
 def summon_captain_planet(planeteer_calls)
-  # Your code here
+  planeteer_calls.map do |ele|
+    ele.capitalize
+  end
 end
 
 def long_planeteer_calls# code an argument here
